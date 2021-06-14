eabf7e7e5bed03612cbc47b2d251e5bcf1671c3a
daegeunkim <daegeunkim92@gmail.com>
2021-06-15 00:19:53 +0900

    1. 암호화하여 계정을 검색하는 작업 2. 암호화 후 콜백 함수에서 err 감지할 때, 회사에서처럼 if(err != null)을 쓰지 않음. if(err) throw err방식

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var account = require('../database/account');
+var crypto = require('crypto');
 var router = express.Router();
 
 router.get('/login', function(req, res, next){
@@ -19,22 +20,23 @@ router.post('/login', function(req, res, next) {
     reason: "고객센터에 문의하세요."
   };
 
+  console.log(userId, userPwd);
+
   if(userId === null || userId === undefined || userId === ''
   || userPwd === null || userPwd === undefined || userPwd === ''){
     result.reason = "ID나 비밀번호를 입력하세요.";
     res.send(result);
   }
   else {
-    crypto.pbkdf2(userPwd, userId, 921118, 64, "sha512", (err, derivedKey) => {
-      if(err !== null || err !== undefined){
+    crypto.pbkdf2(userPwd, 'salt', 100000, 64, "sha512", (err, derivedKey) => {
+      if(err) {
         console.log(err);
         res.send(result);
-        return;
       }
 
-      account.login(userId, derivedKey)
+      account.login(userId, derivedKey.toString('hex'))
       .then((_result) => {
-        if(_result === null || _result === undefined){
+        if(_result === null || _result === undefined || _result.length === 0){
           result.isSuccess = false;
           result.reason = "no account";
         }
